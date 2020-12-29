a8b2b99ba7bf2370117e8ee35c86ac1ced399705
grodrigues851 <72632642+grodrigues851@users.noreply.github.com>
2020-12-29 08:30:35 -0300

    Update rankingGlobal.php
    Atualização Ranking global

--- views/rankingGlobal.php
+++ views/rankingGlobal.php
@@ -5,62 +5,47 @@
                 <img src="img/logoT.png"  alt="Tetris" class="imageRanking">
                 <h1>Ranking Global dos Jogadores</h1>
             </header>
-           <table class="tabelaGlobal">
-                <tr>
-                    <th>Username</th>
-                    <th>Pontuação</th>
-                    <th>Nível máximo</th>
-                </tr>
-                <tr>
-                    <td>RobertinhoZika</td>
-                    <td>550</td>
-                    <td>6</td>
-                <tr>
-                    <td>ElaSumiuDnv</td>
-                    <td>500</td>
-                    <td>6</td>
-                </tr>
-                <tr>
-                    <td>InsoniaMaster</td>
-                    <td>450</td>
-                    <td>6</td>
-                </tr>
-                <tr>
-                    <td>MiadorDeRole</td>
-                    <td>400</td>
-                    <td>6</td>
-                </tr>
-                <tr>
-                    <td>MenosDe170cm</td>
-                    <td>350</td>
-                    <td>6</td>
-                </tr>
-                <tr>
-                    <td>CaraDeAmizadesDuvidosas</td>
-                    <td>300</td>
-                    <td>6</td>
-                </tr>
-                <tr>
-                    <td>OutroCaraComMenosDe170</td>
-                    <td>250</td>
-                    <td>6</td>
-                </tr>
-                <tr>
-                    <td>PedeLogoDemissao</td>
-                    <td>200</td>
-                    <td>6</td>
-                </tr>
-                <tr>
-                    <td>TravouDnv</td>
-                    <td>150</td>
-                    <td>6</td>
-                </tr>
-                <tr>
-                    <td>DaRisadaDaMinhaCara</td>
-                    <td>100</td>
-                    <td>6</td>
-                </tr>
-            </table>
+                
+                
+ <table class="tabelaGlobal">
+                    <tr>
+                        <td width="89">Posição</td>
+                        <td width="142">Nome</td>
+                        <td width="114">Pontuação</td>
+                        <td width="228">Nível Máximo</td>
+
+                    </tr>
+
+                    <?php $posicao = 1; //variavel
+
+                // Inclui o arquivo que faz a conexão ao banco de dados
+                include("includes/mysqli.php");
+
+                //consulta sql
+                $query = mysql_query("SELECT U.usuario, R.pontuacao, R.nivel FROM ranking R
+                                        left join usuario U on U.id = R.idusuario
+
+
+                                         ORDER BY R.pontuacao") or die(mysql_error());
+
+                //faz um looping e cria um array com os campos da consulta
+                while($array = mysql_fetch_array($query)) {
+
+                $nome=$array['usuario'];$pontuacao=$array['pontuacao'];$nivel=$array['nivel']; 
+                ?>
+
+                    <tr>
+                        <td><?php echo $posicao; ?></td>
+                        <td><?php echo $usuario; ?></td>
+                        <td><?php echo $pontuacao; ?></td>
+                        <td><?php echo $nivel; ?></td>
+
+                    </tr>
+
+                    <?php
+                $posicao = $posicao + 1; // acumula proxima posicao ate terminar} ?>
+
+</table>
             <br>
             <b>Posição atual do usuário (nome do usuário aqui): (posição atual). </b> <br> <br>
         </div>
